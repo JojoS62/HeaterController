6398a62233523f553e73ee185134178ec6b29d90
Johannes Stratmann <sn2010@onlinehome.de>
2022-10-31 19:22:46 +0100

    add sdc and fatfs

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,42 @@
 #include "mbed.h"
 #include "lvgl_interface.h"
 
+#include "SDIOBlockDevice.h"
+#include "FATFileSystem.h"
+
+
 DigitalOut  ledRed(LED1, 0);
+
+SDIOBlockDevice bd;
+FATFileSystem fs("sda", &bd);
+
+void print_dir(FileSystem *fs, const char* dirname) {
+    Dir dir(fs, dirname);
+    struct dirent ent;
+
+    // dir.open(fs, dirname);
+    printf("contents of dir: %s\n", dirname);
+    printf("----------------------------------------------------\n");
+
+    while (1) {
+        size_t res = dir.read(&ent);
+        if (0 == res) {
+            break;
+        }
+        printf(ent.d_name);
+        printf("\n");
+    }
+    dir.close();
+}
+
 
 
 int main()
 {
+    // init sdc
+    print_dir(&fs, "/");
+    printf("\n"); 
+
     // init lvgl
     lvgl_interface_init();
 
