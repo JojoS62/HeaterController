11f49065f07819eddf026e1f0d5e00776b545759
Johannes Stratmann <sn2010@onlinehome.de>
2023-02-07 23:22:16 +0100

    make screen scrollable for F746 but it is slow and flickering

--- app_Heatercontroller/src/lv_screens/ui.cpp
+++ app_Heatercontroller/src/lv_screens/ui.cpp
@@ -58,7 +58,7 @@ lv_obj_t * ui_Screen1_Label9;
 void ui_Screen1_screen_init(void)
 {
     ui_Screen1 = lv_obj_create(NULL);
-    lv_obj_clear_flag(ui_Screen1, LV_OBJ_FLAG_SCROLLABLE);      /// Flags
+    // lv_obj_clear_flag(ui_Screen1, LV_OBJ_FLAG_SCROLLABLE);      /// Flags
     lv_obj_set_style_bg_color(ui_Screen1, lv_color_hex(0x525252), LV_PART_MAIN | LV_STATE_DEFAULT);
     lv_obj_set_style_bg_opa(ui_Screen1, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
 
